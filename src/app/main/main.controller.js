10871c11164aa16ddf88b0b6c68f82ef7855729a
seokmin <essemfly@gmail.com>
2014-12-15 01:34:18 +0900

    time spend

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,22 +1,87 @@
 'use strict';
+var tmPromise;
 
 angular.module('toggl')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope','$timeout', function ($scope, $timeout) {
 
+    /*
     $scope.Task = function (start, finish, title, description){
       this.start = start;
       this.finish = finish;
       this.title = title;
       this.description = description;
+    };
+    */
+
+    /*
+    var timeStart = 0, timeEnd =0;
+    $scope.mode = "Start";
+    $scope.timer = "00:00:00";
+
+
+    function checkTime(i) {
+      i = (i < 1) ? 0 : i;
+      if (i < 10) { i = "0" + i; }  // add zero in front of numbers < 10
+      return i;
     }
 
+    function startTimer() {
+      // toggle
+      $scope.mode = "Stop";
+
+      var h, m, s, ms, today = new Date();
+      // compute for the duration,
+      // normalize for the user
+      timeEnd = today.getTime();
+      ms = Math.floor((timeEnd - timeStart) / 1000);
+      h =  checkTime(Math.floor(ms / 3600));
+      ms = Math.floor(ms % 3600);
+      m = checkTime(Math.floor(ms / 60));
+      ms = Math.floor(ms % 60);
+      s = checkTime(Math.floor(ms));
+      // normalize time string
+      $scope.timer = h + ":" + m + ":" + s;
+
+      // timer expired, restart timer
+      tmPromise = $timeout(function () {
+        startTimer();
+      }, 500);
+    }
+
+    function stopTimer() {
+      // toggle
+      $scope.mode = "Start";
+
+      // stop timeout service
+      $timeout.cancel(tmPromise);
+
+      // add to history
+      $scope.timeSchedule.history.push([timeStart,
+        timeEnd,
+        (timeEnd - timeStart) / 1000]);
+    }
+     */
+    $scope.toggleTimer = function () {
+      if($scope.mode ==='Start'){
+        runClock();
+      } else {
+        stopTimer();
+      }
+    };
+
+
+    function checkTime(i) {
+      i = (i < 1) ? 0 : i;
+      if (i < 10) { i = "0" + i; }  // add zero in front of numbers < 10
+      return i;
+    }
 
     $scope.buttonstate = 0;
-    $scope.buttonText = "Start"
+    $scope.buttonText = "Start";
 
     $scope.buttonChange = function(buttonstate){
       if ($scope.buttonstate === 0) {
-        $scope.startTime = Date.now();
+        $scope.runClock();
         $scope.buttonText = "ING";
         $scope.buttonstate += 1;
       } else {
@@ -26,18 +91,38 @@ angular.module('toggl')
       }
     }
 
-    $scope.addItem = function (){
-        $scope.awesomeThings.push({
+    $scope.runClock = function (){
+      var h, m, s, ms, today = new Date();
+      var timeEnd = today.getTime();
+      var timeStart;
+      ms = Math.floor((timeEnd - timeStart) / 1000);
+      h =  checkTime(Math.floor(ms / 3600));
+      ms = Math.floor(ms % 3600);
+      m = checkTime(Math.floor(ms / 60));
+      ms = Math.floor(ms % 60);
+      s = checkTime(Math.floor(ms));
+      // normalize time string
+      $scope.timer = h + ":" + m + ":" + s;
+
+      $scope.startTime = Date.now();
+      $scope.block = Date.now();
+
+
+    }
+
+
+    $scope.addItem = function(){
+        $scope.tasks.push({
           'title': $scope.itemtitle,
           'description': $scope.itemcontent,
           'starttime': $scope.startTime,
           'datesubmit': Date.now(),
-          'logo': 'logo',
-          'timespend':'timespend'
+          'timespend': (Date.now()-$scope.startTime)/1000,
+          'logo': 'logo'
         });
     };
 
-    $scope.awesomeThings = [
+    $scope.tasks = [
       {
         'title': 'Angular UI Bootstrap',
         'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
@@ -55,4 +140,4 @@ angular.module('toggl')
         'timespend': ''
       }
     ];
-  });
+  }]);
