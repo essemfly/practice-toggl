3e796b3748cc45926c045de6d212af86efd36ccc
seokmin <essemfly@gmail.com>
2014-12-14 16:44:01 +0900

    make outline

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,68 +2,59 @@
 
 angular.module('toggl')
   .controller('MainCtrl', function ($scope) {
+    $scope.date =   {
+      Dt: Date.now()
+    };
+    $scope.date = $filter('date')($scope.date, "dd/MM/yyyy");
+
+
+    $scope.buttonstate = 0;
+    $scope.buttonText = "Start"
+
+    $scope.buttonChange = function(buttonstate){
+      if ($scope.buttonstate === 0) {
+        $scope.buttonText = "ING";
+        $scope.buttonstate += 1;
+      } else {
+        $scope.buttonText = "Start";
+        $scope.buttonstate = 0;
+        $scope.addItem();
+      }
+    }
+
+
+    $scope.addItem = function (buttonText, buttonstate){
+
+        $scope.awesomeThings.push({
+          'title': $scope.itemtitle,
+          'description': $scope.itemcontent,
+          'datesubmit': '',
+          'logo': 'logo',
+          'timespend':'timespend'
+        });
+    };
+
+
+
     $scope.awesomeThings = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'jQuery',
-        'url': 'http://jquery.com/',
-        'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
-        'logo': 'jquery.jpg'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
+        'datesubmit': '2014-12-14',
         'title': 'Angular UI Bootstrap',
-        'url': 'http://angular-ui.github.io/bootstrap/',
         'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
-        'logo': 'ui-bootstrap.png'
+        'logo': 'ui-bootstrap.png',
+        'timespend': ''
       },
       {
+        'datesubmit': '2014-12-14',
         'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
         'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
+        'logo': 'node-sass.png',
+        'timespend': ''
       }
     ];
+
+
+
     angular.forEach($scope.awesomeThings, function(awesomeThing) {
       awesomeThing.rank = Math.random();
     });
