a06b6ad1e4e7f50ed9ccdb9b6357e30bbe91ebfa
seokmin <essemfly@gmail.com>
2014-12-15 18:20:28 +0900

    UI-router successgit add .

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,6 @@
 'use strict';
 
-var toggl = angular.module('toggl', ['ngRoute','LocalStorageModule','ui.router']);
-
-
-toggl.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var toggl = angular.module('toggl', ['LocalStorageModule','ui.router']);
 
 
 toggl.config(function(localStorageServiceProvider) {
@@ -20,4 +8,20 @@ toggl.config(function(localStorageServiceProvider) {
     .setPrefix('toggl')
     .setStorageType('sessionStorage')
     .setNotify(true,true);
+});
+
+toggl.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('state1', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('state2', {
+      url: '/state2',
+      templateUrl: 'app/main/b.html',
+      controller: 'bCtrl'
+    });
 });
